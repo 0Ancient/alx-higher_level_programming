a392d75295eeb5544460b4c8f6f61eeeb407d2bf
0Ancient <ndegwagithua@gmail.com>
2023-04-26 08:40:22 +0000

    writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
-
-'use strict';
-
-// Import any required modules here
 const fs = require('fs');
 
-// Define your functions here, if necessary
-function writeStringToFile(file_path, string_to_write) {
-  try {
-    fs.writeFileSync(file_path, string_to_write, 'utf-8');
-  } catch (error) {
-    console.error(error);
-  }
-}
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
 
-// Call your functions here, as necessary
-const file_path = process.argv[2];
-const string_to_write = process.argv[3];
-writeStringToFile(file_path, string_to_write);
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
